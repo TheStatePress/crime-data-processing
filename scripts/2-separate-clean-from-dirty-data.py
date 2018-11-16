f20e1c61eea15071cdb1a949e4a94192081218e6
Chuck Dries <chuck@chuckdries.com>
2018-11-15 19:29:15 -0700

    couting!

--- scripts/2-separate-clean-from-dirty-data.py
+++ scripts/2-separate-clean-from-dirty-data.py
@@ -7,11 +7,42 @@ outputDir = '../2-separated-output/'
 cleanSubDir = 'clean/'
 dirtySubDir = 'dirty/'
 
+dirty = 0
+clean = 0
+empty = 0
+
 def isRowClean(row):
-  return row
+  for value in row:
+    if value == '':
+      return False
+  return True
+
+def isRowEmpty(row):
+  for value in row:
+    if value != '':
+      return False
+  return True
 
 def separateRowsOfFile(fileName):
-  return fileName
+  global dirty
+  global clean
+  global empty
+
+  file = open(inputDir + fileName)
+  reader = csv.reader(file)
+  data = list(reader)
+  for i, row in enumerate(data):
+    if isRowEmpty(row):
+      empty += 1
+    elif not isRowClean(row):
+      dirty += 1
+    else:
+      clean += 1
+
 
 for i, fileName in enumerate(os.listdir(inputDir)):
   separateRowsOfFile(fileName)
+
+print('empty rows: ' + str(empty))
+print('dirty rows: ' + str(dirty))
+print('clean rows: ' + str(clean))
